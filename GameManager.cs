6e0b0099fbfd8520b5757219c7de697b44417751
Mark Joseph Solidarios <bashdev7@gmail.com>
2021-02-04 21:26:06 +0800

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class GameManager : MonoBehaviour
+{
+    public playerA thePlayer;
+   
+    private Vector3 PlayerStartPoint;
+
+    public Text timertxt;
+    public float distanceTimer;
+    public float msec;
+    public float sec;
+    public float min;
+    public bool A;
+    public bool B;
+    public bool simulate;
+
+    public int answer;
+    public string PlayerInputsec;
+    public string PlayerInputmsec;
+    public GameObject second;
+    public GameObject mellisecond;
+    public float finalanswersec;
+    public float finalanswermsec;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        PlayerStartPoint = thePlayer.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (simulate == true)
+        {
+            thePlayer.moveSpeed = 3;
+
+            if (sec == finalanswersec & msec == finalanswermsec)
+            {
+                thePlayer.moveSpeed = 0;
+                StopCoroutine("StopWatch");
+
+            }
+
+        }
+        
+    }
+    public void resetgame()
+    {
+        
+
+    }
+    public void timeStart()
+    {
+        StartCoroutine("StopWatch");
+    }
+    IEnumerator StopWatch()
+    {
+        while (true)
+        {
+            distanceTimer += Time.deltaTime;
+            msec = (int)((distanceTimer - (int)distanceTimer) * 100);
+            sec = (int)(distanceTimer % 60);
+            min = (int)(distanceTimer / 60 % 60);
+            timertxt.text = string.Format("{0:00}:{1:00}:{2:00}", min, sec, msec);
+
+            yield return null;
+        }
+    }
+   
+    public void StoreAnswer()
+    {
+        simulate = true;
+        PlayerInputsec = second.GetComponent<Text>().text;
+        int.TryParse(PlayerInputsec, out answer);
+        finalanswersec = 1 * (float)answer;
+        PlayerInputmsec = mellisecond.GetComponent<Text>().text;
+        int.TryParse(PlayerInputmsec, out answer);
+        finalanswermsec = 1 * (float)answer;
+    }
+   
+}
---
+++ playerA.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerA : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D myRigidbody;
+    private Animator myAnimator;
+
+    public Objectpooler stickman;
+    public GameObject player;
+    public GameObject stickmanpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+        myAnimator.SetFloat("speed", myRigidbody.velocity.x);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == ("stickmanspawn"))
+        {
+            player.SetActive(false);
+            GameObject stick = stickman.GetPooledObject();
+            stick.transform.position = stickmanpoint.transform.position;
+            stick.SetActive(true);
+        }
+
+    }
+}
